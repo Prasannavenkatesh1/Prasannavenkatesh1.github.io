34509fc097ffc3f564ee70fed1d351c4b9a06228
Prasanna <iphonedinesh30@gmail.com>
2022-02-20 05:19:55 +0530

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3ea721c25b41b580979b42889dd5cf18",
-"index.html": "0c3cea0e66caed0820f9561318d4ab35",
-"/": "0c3cea0e66caed0820f9561318d4ab35",
-"main.dart.js": "14359f4ceab3785877e3c9ae9c814c67",
+"index.html": "92225b63a68ce70211ff66f050dda030",
+"/": "92225b63a68ce70211ff66f050dda030",
+"main.dart.js": "2a664b70b6709d79a528d2bb4e4943e5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
